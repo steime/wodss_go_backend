f55a27a2097b0cab01610fb1224ade71be00c7c5
Dominik Steinmann <dominik.steinmann@students.fhnw.ch>
2020-03-24 08:03:49 +0100

    add cors options, allow all origins

--- main.go
+++ main.go
@@ -18,7 +18,10 @@ func main() {
 	fmt.Printf("Server started on port 8080...\n")
 	repository := mySQL.NewMySqlRepository()
 	util.GetAllModules(repository)
-	corsRouter := handlers.CORS()(router.NewRouter(repository))
+	headersOk := handlers.AllowedHeaders([]string{"X-Requested-With"})
+	originsOk := handlers.AllowedOrigins([]string{os.Getenv("ORIGIN_ALLOWED")})
+	methodsOk := handlers.AllowedMethods([]string{"GET", "HEAD", "POST", "PUT", "OPTIONS"})
+	corsRouter := handlers.CORS(originsOk, headersOk, methodsOk)(router.NewRouter(repository))
 	loggedRouter := handlers.LoggingHandler(os.Stdout, corsRouter)
 	log.Fatal(http.ListenAndServe(":8080", loggedRouter))
 
