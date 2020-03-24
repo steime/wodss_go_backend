1045526c7179fd0061373b803938b4caaf7c5eb0
Dominik Steinmann <dominik.steinmann@students.fhnw.ch>
2020-03-24 11:06:47 +0100

    try wildcard for cors headers

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ func main() {
 	fmt.Printf("Server started on port 8080...\n")
 	repository := mySQL.NewMySqlRepository()
 	util.GetAllModules(repository)
-	headersOk := handlers.AllowedHeaders([]string{"X-Requested-With","X-PINGOTHER", "Content-Type"})
+	headersOk := handlers.AllowedHeaders([]string{"*"})
 	originsOk := handlers.AllowedOrigins([]string{os.Getenv("ORIGIN_ALLOWED")})
 	methodsOk := handlers.AllowedMethods([]string{"GET", "HEAD", "POST", "PUT", "OPTIONS"})
 	corsRouter := handlers.CORS(originsOk, headersOk, methodsOk)(router.NewRouter(repository))
