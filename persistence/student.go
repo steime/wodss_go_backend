e8fd4e9abdfb53bda12b593398b2de4094e2bd8f
Dominik Steinmann <dominik.steinmann@students.fhnw.ch>
2020-04-03 12:48:30 +0200

    change creatStudentBody validation, degree to numeric type

--- persistence/student.go
+++ persistence/student.go
@@ -12,7 +12,7 @@ type CreateStudentBody struct {
 	Email string `json:"email,omitempty" validate:"required,email,min=6,max=320"`
 	Password string `json:"password,omitempty" validate:"required,min=10"`
 	Semester string `json:"semester,omitempty" validate:"required"`
-	Degree string `json:"degree,omitempty" validate:"required,alpha"`
+	Degree string `json:"degree,omitempty" validate:"required,numeric"`
 }
 
 
